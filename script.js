ae526fe6013a5ef4384c2fb078888503fc09e685
leyhay <hayleyh9@nycstudents.net>
2026-05-29 13:29:41 +0000

    update3

--- script.js
+++ script.js
@@ -4,4 +4,21 @@ async function init(){
     info = await fetch(link);
     data = await info.json();
     let output = document.getElementById("output");
+    
+    build = "";
+    for(let i=0;i<data.length;i+=1){
+        pool = data[i]
+        build += `<div class=card>
+                     <h3>${pool.permit_type}</h3>
+                     <p>${pool.facility_name}</p>
+                     <p>${pool.facility_name}</p>
+                     <p>${pool.facility_name}</p>
+                     <p>${pool.facility_name}</p>
+                     <hr>
+                     <p>${pool.facility_name}</p>
+                     <p>${pool.facility_name}</p>
+                     <p>${pool.facility_name}</p>
+                </div>`
+    }
+    output.innerHTML = build;
 }
