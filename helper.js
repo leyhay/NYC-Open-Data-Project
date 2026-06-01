215e90b936022fb71e1e7e90f8a1c2ae96a64cf6
leyhay <hayleyh9@nycstudents.net>
2026-06-01 13:32:06 +0000

    update4

--- helper.js
+++ helper.js
@@ -1,3 +1,36 @@
 function get(id){
   return document.getElementById(id);
 }
+function showMap(lat, lon){
+  let location = [lat, lon];
+  if(!mapObj){
+      mapObj = L.map("map");
+  } 
+  let map = mapObj.setView(location, 14);
+
+  const tiles = L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 18,
+    attribution: "&copy; <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a>"
+  }).addTo(map);
+
+  let marker = L.marker(location).addTo(map);
+}
+  function card( info ){ 
+
+  let location = [info.latitude, info.longitude];
+
+  let mapButton = "";
+  if(info.latitude && info.longitude){
+    mapButton = `<input type="button" onclick="showMap( ${location} )" value="Map">`
+  }
+
+  let build = `<div class="card fitted">
+                  <h3>${info.complaint_type}</h3>
+                  <h5>${info.borough}</h5>
+                  <p>${info.incident_zip}</p>
+                  <p>${info.descriptor}</p><hr>
+                  <p>${info.agency_name}<br>(${info.status})</p>
+                  ${mapButton}
+              </div>`;
+  return build;
+}
--- script.js
+++ script.js
@@ -4,21 +4,21 @@ async function init(){
     info = await fetch(link);
     data = await info.json();
     let output = document.getElementById("output");
-    
-    build = "";
-    for(let i=0;i<data.length;i+=1){
-        pool = data[i]
-        build += `<div class=card>
+
+    function card(data){
+      let build = "";
+      build = `<div class="card">
                      <h3>${pool.permit_type}</h3>
                      <p>${pool.facility_name}</p>
-                     <p>${pool.facility_name}</p>
-                     <p>${pool.facility_name}</p>
-                     <p>${pool.facility_name}</p>
+                     <p>${pool.inspection_date}</p>
+                     <p>${pool.address_st}</p>
+                     <p>${pool.bo}</p>
                      <hr>
-                     <p>${pool.facility_name}</p>
-                     <p>${pool.facility_name}</p>
-                     <p>${pool.facility_name}</p>
-                </div>`
+                     <p>${pool.zip}</p>
+                     <p>${pool.of_general_violations}</p>
+                     <p>${pool.lat}</p>
+                     <p>${pool.long}</p>
+                </div>`;
+                return build;
     }
-    output.innerHTML = build;
-}
+  }
