ddb00e41b5366154e3a736ae2a94330e12947d18
leyhay <hayleyh9@nycstudents.net>
2026-06-07 03:57:58 +0000

    update13

--- filter.js
+++ filter.js
@@ -6,20 +6,19 @@ async function filter(){
 
   let output = document.getElementById("output");
   let build = "";
-  for(let i = 0; i < data.length; i+=1){
+  for(let i=0;i<data.length;i+=1){
     let pool = data[i];
     build += `<div class="card">
-                  <h2>Pool Inspections</h2>
-                  Permit type : ${pool.permit_type}<br>
-                  Facility name: ${pool.facility_name}<br>  
-                  Inspection date : ${pool.inspection_date}<br>
-                  Number of violations : ${pool.of_all_violations}<br>
+                  <h3>Facility name: ${pool.facility_name}</h3>
+                  Permit type : ${pool.permit_type} 
+                  Inspection date : ${pool.inspection_date}
+                  Number of violations : ${pool.of_all_violations}
                   <hr>
-                  Borough : ${pool.bo}<br>
-                  Zip code : ${pool.zip}<br>
-                  Latitude : ${pool.lat}<br>
+                  Borough : ${pool.bo}
+                  Zip code : ${pool.zip}
+                  Latitude : ${pool.lat}
                   Longitude: ${pool.long}
-              </div>`;
+              </div>`
   }
   output.innerHTML = build;
 }
